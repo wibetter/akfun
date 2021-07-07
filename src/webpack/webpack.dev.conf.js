5b8d46fd64dabe723c396b771c915c264d8575f1
liudan185 <liudan185@jd.com>
2021-07-07 09:51:12 +0800

    docs: 新增多人协作规范.md

--- src/webpack/webpack.dev.conf.js
+++ src/webpack/webpack.dev.conf.js
@@ -34,7 +34,7 @@ module.exports = () => {
       })
     },
     // cheap-module-eval-source-map is faster for development
-    devtool: '#cheap-module-eval-source-map',
+    devtool: '#source-map', // '#cheap-module-eval-source-map',
     plugins: [
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify(config.dev.NODE_ENV) // vue-router中根据此变量判断执行环境
