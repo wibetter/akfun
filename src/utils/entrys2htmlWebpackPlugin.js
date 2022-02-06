db97a00dc7db66a39f7a4e27a6f2b06bb8e94615
wibetter <365533093@qq.com>
2022-02-06 14:15:50 +0800

    fix: 调整html-webpack-plugin配置项

--- src/utils/entrys2htmlWebpackPlugin.js
+++ src/utils/entrys2htmlWebpackPlugin.js
@@ -24,7 +24,7 @@ module.exports = function (entryConfig, curDefaultHtmlTemplate) {
             filename: `${filename}.html`,
             template: curPageTemplate,
             chunks: ['vendor', 'common', filename],
-            inject: true, // 当传递true或body时，所有javascript资源都将放置在body元素的底部。
+            inject: 'body', // 当传递true或body时，所有javascript资源都将放置在body元素的底部。
             minify: false, // mode: 'production'模式下会自定压缩html代码，优先级比minify高
             // necessary to consistently work with multiple chunks via CommonsChunkPlugin
             chunksSortMode: 'auto'
