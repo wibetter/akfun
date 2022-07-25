ba440fd5ff1f967469bfb247ec20a8243768a2b9
wibetter <365533093@qq.com>
2022-07-25 23:36:29 +0800

    fix: 补上丢失的loader

--- src/utils/pathUtils.js
+++ src/utils/pathUtils.js
@@ -4,7 +4,8 @@ const fs = require('fs');
 // 当前命令执行的路径
 const currentRoot = () => fs.realpathSync(process.cwd());
 const resolveToCurrentRoot = (filePath) => path.resolve(currentRoot(), filePath);
-const resolveToCurrentDist = (filePath) => path.resolve(currentRoot(), 'dist/', filePath);
+const resolveToCurrentDist = (filePath, outDir) =>
+  path.resolve(currentRoot(), outDir || 'dist/', filePath);
 const currentSrc = () => resolveToCurrentRoot('src');
 const currentBuild = () => resolveToCurrentRoot('build');
 // 获取当前项目的package文件（从当前命令执行的路径下查找）
