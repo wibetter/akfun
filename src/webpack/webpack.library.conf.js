5582b4763fd8db0784a40122626a44f3d117190b
wibetter <365533093@qq.com>
2022-08-02 12:01:20 +0800

    fix(build2lib): 支持默认的libraryName

--- src/webpack/webpack.library.conf.js
+++ src/webpack/webpack.library.conf.js
@@ -23,7 +23,7 @@ module.exports = (akfunConfig) => {
       publicPath: '',
       library: {
         type: 'umd', // 定义打包方式Universal Module Definition,同时支持在CommonJS、AMD和全局变量使用
-        name: curEnvConfig.libraryName
+        name: curEnvConfig.libraryName || '[name]'
       },
       // 指定类库名,主要用于直接引用的方式(比如使用script 标签)
       globalObject: 'this' // 定义全局变量,兼容node和浏览器运行，避免出现"window is not defined"的情况
