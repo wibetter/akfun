72b5c728eb0fc9558e0f08a02e7cf75bba3bb598
wibetter <365533093@qq.com>
2024-08-10 18:58:36 +0800

    fix: 避免webpack NODE_ENV配置失效

--- src/webpack/webpack.library.conf.js
+++ src/webpack/webpack.library.conf.js
@@ -53,7 +53,7 @@ module.exports = (akfunConfig) => {
        */
       chunkIds: 'named',
       emitOnErrors: true,
-      minimize: true,
+      // minimize: true,
       minimizer: [
         // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
         `...`,
